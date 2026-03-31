e518972701a8f7865f5f2aceba9aa36ee7ec1030
coffeeandwork <6377739+coffeeandwork@users.noreply.github.com>
2026-03-31 08:46:59 -0400

    Fix copyright year 2024 → 2026 in LICENSE, LICENSE_HEADER, NOTICE, and all source files

--- plugin/src/main/java/dev/protocgen/textcodecs/pbtkurl/codegen/python/PbtkPythonGenerator.java
+++ plugin/src/main/java/dev/protocgen/textcodecs/pbtkurl/codegen/python/PbtkPythonGenerator.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2024 protobuf-text-codecs contributors
+ * Copyright 2026 protobuf-text-codecs contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
